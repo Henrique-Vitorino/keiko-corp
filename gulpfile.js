fa05acdf3c75cd58698e12f8b1578bb32cafb960
Henrique <henriquevitorino19@gmail.com>
2022-04-25 22:21:25 -0300

    Alterado as jpg

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require("gulp");
 
 var webp = require("gulp-webp");
+
 function gulpImage() {
-  return gulp.src("img/*.png").pipe(webp()).pipe(gulp.dest("dist/img"));
+  return gulp
+    .src(["img/*.png", "img/*.jpg"])
+    .pipe(webp())
+    .pipe(gulp.dest("dist/img"));
 }
 
 gulp.task("default", gulp.parallel(gulpImage));
